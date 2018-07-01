88775a08d3b967afb9e0b390e727203a027d4c4c
Emre Cürgül <pongo1999712@gmail.com>
2018-07-01 21:27:35 +0200

    Re-introduce dist folder

---
+++ dist/__resource.lua
@@ -0,0 +1,14 @@
+resource_manifest_version "05cfa83c-a124-4cfa-a768-c24a5811d8f9"
+
+files {
+	"Newtonsoft.Json.dll"
+}
+
+client_scripts {
+	"nativeui.net.dll",
+	"freeroam.net.dll"
+}
+
+server_script "freeroamserver.net.dll"
+
+shared_script "freeroamshared.net.dll"
